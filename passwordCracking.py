8c8aa9a9307e931b68d7aab0dfb5bb021630c804
WilliamDini <112749345+WilliamDini@users.noreply.github.com>
2025-04-10 23:21:43 -0700

    Update passwordCracking.py

--- passwordCracking.py
+++ passwordCracking.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import platform
 import sys
 import crypt
 from multiprocessing import Pool, cpu_count
@@ -61,13 +62,39 @@ saltValue = '$' + mashLocated[1] + '$' + mashLocated[2] + '$'
 
 #---------------------------------
 def crackTheCode(passLetter):
+    start = time.time()
     alpha = 'abcdefghijklmnopqrstuvwxyz'
-    combination = itertools.product(alpha, repeat=7)
+    combination = itertools.product(alpha, repeat=5)
     print(f'Starting Cracking on: {passLetter}*****')
+    passChecks = 0
     for c in combination:
         passes = passLetter + ''.join(c)
+        passChecks += 1
         if crypt.crypt(passes, saltValue) == passValue:
             print('\nPASSWORD CRACKED! ~ SUCCESS! :', passes)
+            
+            num_processes = cpu_count()
+            print(f"\nNumber of Processes/Threads: {num_processes}")
+
+            cpu_model = platform.processor()
+            print(f"CPU Model: {cpu_model}")
+
+            # print("\nSource Code:")
+            # with open(__file__, 'r') as f:
+            #     print(f.read())
+            
+            print(f"Number of Checks: {passChecks}")
+            
+            end = time.time()
+            seconds = end - start
+            throughput = passChecks / float(seconds)
+            
+            print(f"Throughput: {throughput}")
+            
+            print(f"\nTime to Crack: {seconds} seconds\n")
+            
+            
+            
             return passes
     # print('FAILED CRACK!')
     return None
@@ -76,20 +103,15 @@ def crackTheCode(passLetter):
 #---------------------------------
 print("\nCracking Password...")
 
-start = time.time()
-
 alpha = 'abcdefghijklmnopqrstuvwxyz'
 pool = Pool(cpu_count())
 with Pool(processes=26) as pool:
     password = pool.map(crackTheCode, alpha)
 
-    for p in password:
-        if p:
-            print("Password for", desiredCrack, ":", p)
-            end = time.time()
-            print(f"Elapsed time: {end - start} seconds\n")
+    # for p in password:
+    #     if p:
+    #         print("Password for", desiredCrack, ":", p)
 #---------------------------------
 
 #'team11', '$1$SXA89G4E$pZ1EOTFJ14m7TihlI8SrW.', '16653', '0', '99999', '7'       
-# Team 11 Password : dcacgx 
-# Bonus Password : 
+# Team 11 Password : dcacgx  
